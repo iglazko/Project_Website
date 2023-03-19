317c2fe80052c75ffc3bfcbdd62d265987734c74
iglazko <iglazko@sfedu.ru>
2023-03-19 19:45:27 +0300

    FIRST

--- src/components/head/Head.js
+++ src/components/head/Head.js
@@ -3,23 +3,9 @@ import {Link} from "react-router-dom";
 
 function Head(){
     return(
-        <div className = "head_content">
-            <div className = "news">
-                <Link to = {'/news'}>
-                <h3>Новости</h3>
-                </Link>
-            </div>
-            <div className = "contacts">
-                <Link to = {'/contacts'}>
-                <h3>Контакты</h3>
-                </Link>
-            </div>
-            <div className = "about">
-                <Link to = {'/about'}>
-                <h3>О проекте</h3>
-                </Link>
-            </div>
+        <div>
+        <h1>Занимательная информатика в развитии логического мышления </h1>
         </div>
-    );
+)
 }
 export default Head;
